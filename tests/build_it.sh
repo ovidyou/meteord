fa9ba964464ff29ef7bebff844645a4d4952afd5
Ovi Indrei <ovidiu.indrei@gmail.com>
2017-05-10 13:14:40 +0200

    Replace token 'abernix' with 'ovidyou'

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,11 +3,11 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "ovidyou/meteord:node-${NODE_VERSION}-base" ../base && \
+  docker tag "ovidyou/meteord:node-${NODE_VERSION}-base" ovidyou/meteord:base
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "ovidyou/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
+  docker tag "ovidyou/meteord:node-${NODE_VERSION}-onbuild" ovidyou/meteord:onbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "ovidyou/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
+  docker tag "ovidyou/meteord:node-${NODE_VERSION}-devbuild" ovidyou/meteord:devbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "ovidyou/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
+  docker tag "ovidyou/meteord:node-${NODE_VERSION}-binbuild" ovidyou/meteord:binbuild
